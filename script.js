3ef105f448215680916f552eab654dc8dd1c36d3
tyler <tyler.tongseng@gmail.com>
2022-12-20 17:49:54 +0800

    Add Reset Button

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let score = 0;
 const winScoreP1 = 10; // player 1 range 0~10 "win" at 10
 const winScoreP2 = -10; // player 2 range 0~ -10 "win" at -10
 const scoreDisplay = document.querySelector("h3"); // display current the score
-let str = 1000; // lower the number mean higher str
+var str = 10; // lower the number mean higher str
 let win = 0; // count user winning streak
 // let count = 0;
 
@@ -73,30 +73,31 @@ const nextLevel = () => {
   win++;
   switch (win) {
     case 0:
-      str = str;
+      str = 100;
       console.log(`${str} case 1000`);
       break;
     case 1:
-      str = 800;
+      str = 100;
       console.log(`${str} case 800`);
       break;
     case 2:
-      str = 600;
-      console.log(`${str} case 600`);
-      break;
-    case 3:
       str = 400;
       console.log(`${str} case 400`);
       break;
-    case 4:
+    case 3:
       str = 300;
       console.log(`${str} case 300`);
       break;
-    case 5:
+    case 4:
       str = 200;
       console.log(`${str} case 200`);
       break;
+    case 5:
+      str = 100;
+      console.log(`${str} case 100`);
+      break;
     default:
+      console.log(str);
       console.log(win);
       console.log(score + "what is current score");
       console.log(winScoreP1 + "winScoreP1");
